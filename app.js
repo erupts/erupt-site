e38eabf77e634e4cbbc6658145ebb4d2c9eee267
122339792@qq.com <122339792@qq.com>
2019-10-24 18:39:53 +0800

    init

---
+++ app.js
@@ -0,0 +1,59 @@
+function toTemplate() {
+    window.scrollTo(0, 0);
+}
+
+var app = angular.module('app', [
+    'ngRoute'
+]).run(function ($rootScope) {
+    $rootScope.year = new Date().getFullYear();
+}).config(function ($provide, $compileProvider, $controllerProvider, $filterProvider) {
+    app.controller = $controllerProvider.register;
+    app.directive = $compileProvider.directive;
+    app.filter = $filterProvider.register;
+    app.factory = $provide.factory;
+    app.service = $provide.service;
+    app.constant = $provide.constant;
+}).config(function ($routeProvider) {
+    $routeProvider.when('/:name.html', {
+        templateUrl: function (attr) {
+            toTemplate();
+            return "page/" + attr.name + ".html";
+        },
+        resolve: {
+            load: function ($q, $route, $rootScope) {
+                var deferred = $q.defer();
+                var params = $route.current.params;
+                var dependencies = [
+                    'page/' + params.name + '.js'
+                ];
+                $script(dependencies, function () {
+                    $rootScope.$apply(function () {
+                        deferred.resolve();
+                    });
+                });
+                return deferred.promise;
+            }
+        }
+    }).when('/:dir/:name.html', {
+        templateUrl: function (attr) {
+            toTemplate();
+            return "page/" + attr.dir + "/" + attr.name + ".html";
+        }, resolve: {
+            load: function ($q, $route, $rootScope) {
+                var deferred = $q.defer();
+                var params = $route.current.params;
+                var dependencies = [
+                    "page/" + params.dir + "/" + params.name + ".js"
+                ];
+                $script(dependencies, function () {
+                    $rootScope.$apply(function () {
+                        deferred.resolve();
+                    });
+                });
+                return deferred.promise;
+            }
+        }
+    }).otherwise({
+        redirectTo: 'home.html'
+    });
+})
