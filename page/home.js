95ed0e3421dc6f0e625bb3282431f3419f5f39e7
122339792@qq.com <122339792@qq.com>
2019-10-24 19:01:23 +0800

    初始化文档基本信息

--- page/home.js
+++ page/home.js
@@ -1,3 +1,19 @@
 app.controller("home", function ($scope) {
-
+    $scope.erupt = [
+        {annotation: 'primaryKeyCol'},
+        {annotation: 'name'},
+        {annotation: 'desc'},
+        {annotation: 'loginUse'},
+        {annotation: '@rowOperation[]'},
+        {annotation: '@Filter'},
+        {annotation: '@orderBy'},
+        {annotation: 'dataProxy'},
+        {annotation: '@Tree'},
+        {annotation: '@param'},
+    ]
+    $scope.eruptField = [
+        {annotation: '@View'},
+        {annotation: '@Edit'},
+        {annotation: '@params'},
+    ]
 });
