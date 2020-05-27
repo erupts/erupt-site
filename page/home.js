42e096c221141aaaccbb63c99fb128b5ee7b3e78
liyuepeng <liyuepeng>
2020-05-28 00:07:01 +0800

    更新整体布局

--- page/home.js
+++ page/home.js
@@ -1,54 +1,67 @@
 app.controller("home", function ($scope) {
     $scope.erupt = [
-        {annotation: 'primaryKeyCol'},
-        {annotation: 'name'},
-        {annotation: 'desc'},
-        {annotation: 'loginUse'},
-        {annotation: 'rowOperation'},
-        {annotation: 'filter'},
-        {annotation: 'orderBy'},
-        {annotation: 'dataProxy'},
-        {annotation: 'tree'},
-        {annotation: 'param'},
-    ];
-    $scope.eruptField = [
-        {annotation: '@View'},
-        {annotation: '@Edit'},
-        {annotation: '@params'},
+        {annotation: '@Erupt'},
+        {annotation: '@EruptField'},
+        {annotation: '@EruptRouter'},
+        {annotation: '@EruptApi'},
+        {annotation: '@EruptTpl'},
+        {annotation: '@TplAction'},
+        {annotation: '@EruptProperty'},
     ];
 
-    $scope.view = [
-        {annotation: 'title'},
-        {annotation: 'column'},
-        {annotation: 'viewType'},
-        {annotation: 'show'},
-        {annotation: 'sortable'},
-        {annotation: 'export'},
-        {annotation: 'className'},
-        {annotation: 'template'},
-    ];
 
-    $scope.edit = [
-        {annotation: 'title'},
-        {annotation: 'desc'},
-        {annotation: 'notNull'},
-        {annotation: 'show'},
-        {annotation: 'readOnly'},
-        {annotation: 'placeHolder'},
-        {annotation: 'search'},
-        {annotation: 'orderBy'},
-        {annotation: 'filter'},
-        {annotation: 'type'},
-        {annotation: 'inputType'},
-        {annotation: 'numberType'},
-        {annotation: 'boolType'},
-        {annotation: 'htmlType'},
-        {annotation: 'choiceType'},
-        {annotation: 'dateType'},
-        {annotation: 'sliderType'},
-        {annotation: 'attachmentType'},
-        {annotation: 'dependSwitchType'},
-        {annotation: 'referenceTreeType'},
-        {annotation: 'referenceTableType'},
-    ];
+    // $scope.erupt = [
+    //     {annotation: 'primaryKeyCol'},
+    //     {annotation: 'name'},
+    //     {annotation: 'desc'},
+    //     {annotation: 'loginUse'},
+    //     {annotation: 'rowOperation'},
+    //     {annotation: 'filter'},
+    //     {annotation: 'orderBy'},
+    //     {annotation: 'dataProxy'},
+    //     {annotation: 'tree'},
+    //     {annotation: 'param'},
+    // ];
+    //
+    //
+    // $scope.eruptField = [
+    //     {annotation: '@View'},
+    //     {annotation: '@Edit'},
+    //     {annotation: '@params'},
+    // ];
+    //
+    // $scope.view = [
+    //     {annotation: 'title'},
+    //     {annotation: 'column'},
+    //     {annotation: 'viewType'},
+    //     {annotation: 'show'},
+    //     {annotation: 'sortable'},
+    //     {annotation: 'export'},
+    //     {annotation: 'className'},
+    //     {annotation: 'template'},
+    // ];
+    //
+    // $scope.edit = [
+    //     {annotation: 'title'},
+    //     {annotation: 'desc'},
+    //     {annotation: 'notNull'},
+    //     {annotation: 'show'},
+    //     {annotation: 'readOnly'},
+    //     {annotation: 'placeHolder'},
+    //     {annotation: 'search'},
+    //     {annotation: 'orderBy'},
+    //     {annotation: 'filter'},
+    //     {annotation: 'type'},
+    //     {annotation: 'inputType'},
+    //     {annotation: 'numberType'},
+    //     {annotation: 'boolType'},
+    //     {annotation: 'htmlType'},
+    //     {annotation: 'choiceType'},
+    //     {annotation: 'dateType'},
+    //     {annotation: 'sliderType'},
+    //     {annotation: 'attachmentType'},
+    //     {annotation: 'dependSwitchType'},
+    //     {annotation: 'referenceTreeType'},
+    //     {annotation: 'referenceTableType'},
+    // ];
 });
