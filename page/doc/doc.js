5345e9eab06c75ca9cf0b1520796e7304c997dbd
yuepeng <erupts@126.com>
2021-05-13 19:22:56 +0800

    update doc version switch

--- page/doc/doc.js
+++ page/doc/doc.js
@@ -1,5 +1,7 @@
-app.controller("doc", function ($scope, $rootScope, $timeout) {
+app.controller("doc", function ($scope, $rootScope, $location, $sce) {
     $scope.loading = true;
+    $scope.url = $sce.trustAsResourceUrl("https://www.yuque.com/erupts" + $location.search().link);
+    console.log($scope.url)
     angular.element(document.getElementById("frame-doc")).bind('load', function () {
         $scope.loading = false
         $scope.$apply();
